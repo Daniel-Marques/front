94c09ad2c1038cd7cd862a8792e48321d2f45507
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-02 14:44:18 -0300

    Styled profile

--- src/components/Saudation/index.tsx
+++ src/components/Saudation/index.tsx
@@ -9,7 +9,7 @@ interface UserNameProps {
 
 const Saudation: React.FC<UserNameProps> = ({ title, subtitle }) => {
   return (
-    <SaudationDiv className="typography-hd-cr-3 mb-5">
+    <SaudationDiv className="typography-hd-cr-3 mb-3">
       <SaudationTitle style={{ color: "grey" }}>
         <span style={{ fontWeight: "lighter" }}>{title} </span>
         <strong>{subtitle}</strong>
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -53,135 +53,151 @@ const Profile: React.FC = () => {
             <Saudation title="" subtitle="Meu Perfil" />
           </div>
 
-          <div className="col-12">
-            <Form ref={formRef} onSubmit={() => {}}>
-              <div className="row">
-                <div className="col-lg-12 mb-3">
-                  <label>Nome do Usuário</label>
-                  <Input
-                    name="name"
-                    placeholder="Digite o nome do usuário"
-                    required
-                    defaultValue={name}
-                  />
-                </div>
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="card-body">
+                <Form ref={formRef} onSubmit={() => {}}>
+                  <div className="row">
+                    <div className="col-lg-12 mb-3">
+                      <label>Nome do Usuário</label>
+                      <Input
+                        name="name"
+                        placeholder="Digite o nome do usuário"
+                        required
+                        defaultValue={name}
+                      />
+                    </div>
 
-                <div className="col-lg-6 mb-3">
-                  <label>CPF</label>
-                  <Input
-                    name="document"
-                    placeholder="Ex: 000.000.000-00"
-                    required
-                    defaultValue={document}
-                  />
-                </div>
+                    <div className="col-lg-6 mb-3">
+                      <label>CPF</label>
+                      <Input
+                        name="document"
+                        placeholder="Ex: 000.000.000-00"
+                        required
+                        defaultValue={document}
+                      />
+                    </div>
 
-                <div className="col-lg-6 mb-3">
-                  <label>Número do PIS</label>
-                  <Input name="pis" placeholder="Ex: 000.00000.00-0" defaultValue={pis} />
-                </div>
+                    <div className="col-lg-6 mb-3">
+                      <label>Número do PIS</label>
+                      <Input
+                        name="pis"
+                        placeholder="Ex: 000.00000.00-0"
+                        defaultValue={pis}
+                      />
+                    </div>
 
-                <div className="col-lg-6 mb-3">
-                  <label>Email</label>
-                  <Input
-                    name="email"
-                    type="email"
-                    placeholder="Informe aqui o E-mail"
-                    required
-                    defaultValue={email}
-                  />
-                </div>
+                    <div className="col-lg-6 mb-3">
+                      <label>Email</label>
+                      <Input
+                        name="email"
+                        type="email"
+                        placeholder="Informe aqui o E-mail"
+                        required
+                        defaultValue={email}
+                      />
+                    </div>
 
-                <div className="col-lg-6 mb-3">
-                  <label>Senha</label>
-                  <Input
-                    name="password"
-                    type="password"
-                    placeholder="Informe aqui a senha"
-                    required
-                  />
-                </div>
+                    <div className="col-lg-6 mb-3">
+                      <label>Senha</label>
+                      <Input
+                        name="password"
+                        type="password"
+                        placeholder="Informe aqui a senha"
+                        required
+                      />
+                    </div>
 
-                <div className="col-lg-4 mb-3">
-                  <label>CEP</label>
-                  <Input
-                    type="number"
-                    name="zipcode"
-                    // onChange={handleChange}
-                    placeholder="Qual o CEP?"
-                    defaultValue={zipcode}
-                  />
-                </div>
+                    <div className="col-lg-4 mb-3">
+                      <label>CEP</label>
+                      <Input
+                        type="number"
+                        name="zipcode"
+                        // onChange={handleChange}
+                        placeholder="Qual o CEP?"
+                        defaultValue={zipcode}
+                      />
+                    </div>
 
-                <div className="col-lg-8 mb-3">
-                  <label>Endereço</label>
-                  <Input
-                    name="address"
-                    // value={`${address}`}
-                    // onChange={(e) => setAddress(e.target.value)}
-                    placeholder="Endereço"
-                    defaultValue={address}
-                  />
-                </div>
+                    <div className="col-lg-8 mb-3">
+                      <label>Endereço</label>
+                      <Input
+                        name="address"
+                        // value={`${address}`}
+                        // onChange={(e) => setAddress(e.target.value)}
+                        placeholder="Endereço"
+                        defaultValue={address}
+                      />
+                    </div>
 
-                <div className="col-lg-4 mb-3">
-                  <label>Número</label>
-                  <Input name="number" placeholder="Ex.: 123 ou S/N" defaultValue={number} />
-                </div>
+                    <div className="col-lg-4 mb-3">
+                      <label>Número</label>
+                      <Input
+                        name="number"
+                        placeholder="Ex.: 123 ou S/N"
+                        defaultValue={number}
+                      />
+                    </div>
 
-                <div className="col-lg-8 mb-3">
-                  <label>Complemento</label>
-                  <Input name="complement" placeholder="Ex.: Casa" defaultValue={complement}/>
-                </div>
+                    <div className="col-lg-8 mb-3">
+                      <label>Complemento</label>
+                      <Input
+                        name="complement"
+                        placeholder="Ex.: Casa"
+                        defaultValue={complement}
+                      />
+                    </div>
 
-                <div className="col-lg-4 mb-3">
-                  <label>Cidade</label>
-                  <Input
-                    name="city"
-                    placeholder="Ex.: Russas"
-                    defaultValue={city}
-                    // value={`${city}`}
-                    // onChange={(e) => setCity(e.target.value)}
-                  />
-                </div>
+                    <div className="col-lg-4 mb-3">
+                      <label>Cidade</label>
+                      <Input
+                        name="city"
+                        placeholder="Ex.: Russas"
+                        defaultValue={city}
+                        // value={`${city}`}
+                        // onChange={(e) => setCity(e.target.value)}
+                      />
+                    </div>
 
-                <div className="col-lg-4 mb-3">
-                  <label>UF</label>
-                  <Input
-                    name="state"
-                    placeholder="Ex.: CE"
-                    defaultValue={uf}
-                    // value={`${uf}`}
-                    // onChange={(e) => setUf(e.target.value)}
-                  />
-                </div>
+                    <div className="col-lg-4 mb-3">
+                      <label>UF</label>
+                      <Input
+                        name="state"
+                        placeholder="Ex.: CE"
+                        defaultValue={uf}
+                        // value={`${uf}`}
+                        // onChange={(e) => setUf(e.target.value)}
+                      />
+                    </div>
 
-                <div className="col-lg-4 mb-3">
-                  <label>País</label>
-                  <Input
-                    name="country"
-                    placeholder="Ex.: Brasil"
-                    defaultValue={country}
-                    // value={`${country}`}
-                    // onChange={(e) => setCountry(e.target.value)}
-                  />
-                </div>
+                    <div className="col-lg-4 mb-3">
+                      <label>País</label>
+                      <Input
+                        name="country"
+                        placeholder="Ex.: Brasil"
+                        defaultValue={country}
+                        // value={`${country}`}
+                        // onChange={(e) => setCountry(e.target.value)}
+                      />
+                    </div>
 
-                <div className="col-12">
-                  <button
-                    type="submit"
-                    className="btn btn-labeled btn-primary"
-                    data-testid="edit-user-button"
-                    style={{ marginLeft: 12, borderRadius: 3, height: 40 }}
-                  >
-                    <span className="btn-label" style={{ height: 40 }}>
-                      <i className="fa fa-check"></i>
-                    </span>
-                    Editar usuário
-                  </button>
-                </div>
+                    <div className="col-12">
+                      <button
+                        type="submit"
+                        className="btn btn-labeled btn-primary"
+                        data-testid="edit-user-button"
+                        style={{ marginLeft: 12, borderRadius: 3, height: 40 }}
+                      >
+                        <span className="btn-label" style={{ height: 40 }}>
+                          <i className="fa fa-check"></i>
+                        </span>
+                        Editar usuário
+                      </button>
+                    </div>
+                  </div>
+                </Form>
               </div>
-            </Form>
+            </div>
           </div>
         </div>
       </div>

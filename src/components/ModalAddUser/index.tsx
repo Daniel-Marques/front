f974021057c96dea8d95d47ddf7137ae93d09286
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-03 08:54:39 -0300

    Fix Css

--- src/components/ModalAddUser/index.tsx
+++ src/components/ModalAddUser/index.tsx
@@ -91,22 +91,22 @@ const ModalAddUser: React.FC<IModalProps> = ({
       <Form ref={formRef} onSubmit={handleSubmit}>
         <h3>Novo usuário</h3>
         <div className="row">
-          <div className="col-lg-12 mb-3">
+          <div className="col-lg-12" style={{ marginBottom: 5 }}>
             <label>Nome do Usuário</label>
             <Input name="name" placeholder="Digite o nome do usuário" />
           </div>
 
-          <div className="col-lg-6 mb-3">
+          <div className="col-lg-6" style={{ marginBottom: 5 }}>
             <label>CPF</label>
             <Input name="document" placeholder="Ex: 000.000.000-00" />
           </div>
 
-          <div className="col-lg-6 mb-3">
+          <div className="col-lg-6" style={{ marginBottom: 5 }}>
             <label>Número do PIS</label>
             <Input name="pis" placeholder="Ex: 000.00000.00-0" />
           </div>
 
-          <div className="col-lg-6 mb-3">
+          <div className="col-lg-6" style={{ marginBottom: 5 }}>
             <label>Email</label>
             <Input
               name="email"
@@ -115,7 +115,7 @@ const ModalAddUser: React.FC<IModalProps> = ({
             />
           </div>
 
-          <div className="col-lg-6 mb-3">
+          <div className="col-lg-6" style={{ marginBottom: 5 }}>
             <label>Senha</label>
             <Input
               name="password"
@@ -124,37 +124,37 @@ const ModalAddUser: React.FC<IModalProps> = ({
             />
           </div>
 
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-4" style={{ marginBottom: 5 }}>
             <label>CEP</label>
             <Input type="number" name="zipcode" placeholder="Qual o CEP?" />
           </div>
 
-          <div className="col-lg-8 mb-3">
+          <div className="col-lg-8" style={{ marginBottom: 5 }}>
             <label>Endereço</label>
             <Input name="address" placeholder="Endereço" />
           </div>
 
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-4" style={{ marginBottom: 5 }}>
             <label>Número</label>
             <Input name="number" placeholder="Ex.: 123 ou S/N" />
           </div>
 
-          <div className="col-lg-8 mb-3">
+          <div className="col-lg-8" style={{ marginBottom: 5 }}>
             <label>Complemento</label>
             <Input name="complement" placeholder="Ex.: Casa" />
           </div>
 
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-4" style={{ marginBottom: 5 }}>
             <label>Cidade</label>
             <Input name="city" placeholder="Ex.: Russas" />
           </div>
 
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-4" style={{ marginBottom: 5 }}>
             <label>UF</label>
             <Input name="state" placeholder="Ex.: CE" />
           </div>
 
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-4" style={{ marginBottom: 5 }}>
             <label>País</label>
             <Input name="country" placeholder="Ex.: Brasil" />
           </div>

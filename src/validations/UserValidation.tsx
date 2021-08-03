c0647a64249f938e499e64cd212e556ef7dfa8d7
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-03 10:56:23 -0300

    Fix Validation

--- src/validations/UserValidation.tsx
+++ src/validations/UserValidation.tsx
@@ -10,40 +10,26 @@ export const userSchema = yup.object().shape({
     .string()
     .length(11, "CPF deve conter 11 caracteres")
     .required("O campo é obrigatório."),
-    // .test(
-    //   "unique-document",
-    //   "CPF já cadastrado em nossa base",
-    //   async function (value) {
-    //     const { document } = this.parent;
-    //     const response = await api.get(`/users/document/${document}`);
-    //     if (response.data === false) {
-    //       return true;
-    //     }
-    //     return false;
-    //   }
-    // ),
+  // ,tw  1234567
   pis: yup.string().required("O campo é obrigatório"),
   email: yup
     .string()
     .email("Informe um e-mail válido")
     .required("O campo é obrigatório"),
-    // .test(
-      // "unique-email",
-      // "E-mail já cadastrado em nossa base",
-      // async function (value) {
-      //   const { email } = this.parent;
-      //   const response = await api.get(`/users/email/${email}`);
-      //   if (response.data === false) {
-      //     return true;
-      //   }
-      //   return false;
-      // }
-    // ),
+  // .test(
+  //   "unique-email",
+  //   "E-mail já cadastrado em nossa base",
+  //   async function (value) {
+  //     const { email } = this.parent;
+  //     const response = await api.get(`/users/email/${email}`);
+  //     if (response.data === false) {
+  //       return true;
+  //     }
+  //     return false;
+  //   }
+  // ),
   password: yup.string().required("O campo é obrigatório"),
-  zipcode: yup
-    .number()
-    .typeError("Você deve informar um número")
-    .required("O campo é obrigatório"),
+  zipcode: yup.string().required("O campo é obrigatório"),
   address: yup.string().required("O campo é obrigatório"),
   number: yup.string(),
   complement: yup.string(),
